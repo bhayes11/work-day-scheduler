a898518c0300c94abb33c22617159bda167b8d7e
Hayes11 <bhayes81@gmail.com>
2020-10-04 13:43:32 -0400

    added document.ready function

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-//console.log("hello");
+$(document).ready(function () {
+  //console.log("hello");
+});
+
 $("#currentDay").text(moment().format("dddd MMMM Do YYYY, h:mm a"));
+$(".container").append("<div>hi</div>");
