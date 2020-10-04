3fb392ae5220c71ff2efeac946826ce2603f5616
Hayes11 <bhayes81@gmail.com>
2020-10-04 15:00:44 -0400

    added pseudo code

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
   //console.log("hello");
+  //Dom Variables
+  //JS Variables
+  //Function Definitions
+  //Function Calls
+  //Event Listeners
+  $("#currentDay").text(moment().format("dddd MMMM Do YYYY, h:mm a"));
+  $(".container").append("<div>hi</div>");
 });
 
-$("#currentDay").text(moment().format("dddd MMMM Do YYYY, h:mm a"));
-$(".container").append("<div>hi</div>");
+//Div for Day, input field & button.
+//loop through array of numbers
+//if < currentHour then gray, if = currentHour then red, if > currentHour then green
