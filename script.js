e20f72df884473c11b0a3776d537def2e29edc73
Hayes11 <bhayes81@gmail.com>
2020-10-06 00:16:52 -0400

    deleted HTML body structure and created body structure in jQuery

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-//array
-var time = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-for (var i = 0; i < time.length; i++) {
-  var timeStorage = localStorage.getItem(time[i]);
-  console.log(time);
-}
-
 $(document).ready(function () {
+  //array
+  var time = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+  for (var i = 0; i < time.length; i++) {
+    var timeStorage = localStorage.getItem(time[i]);
+    console.log(time);
+  }
+
+  var row = $("<div>");
+  var hour = $("<div>");
+  var description = $("<textarea>");
+  var saveBtn = $("<button>");
+  hour.addClass("hour col-sm-1");
+
   //console.log("hello");
   //Dom Variables
   //JS Variables
