1ade922251c15409d94e2d942246062eba7e0f25
Hayes11 <bhayes81@gmail.com>
2020-10-07 18:05:16 -0400

    added var currentTime and changed if else by using correct variable

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
   $("#currentDay").text(moment().format("dddd MMMM Do YYYY, h:mm a"));
-
+  var currentTime = parseInt(moment().format("HH"));
+  //var currentTime = moment().format("HH");
   //array
   var timeDisplay = [9, 10, 11, 12, 13, 14, 15, 16, 17];
   for (var i = 0; i < timeDisplay.length; i++) {
     //var timeStorage = localStorage.getItem(time[i]);
-    console.log(timeDisplay);
+    console.log(timeDisplay[i]);
 
     //Variables for the function
 
@@ -17,14 +18,14 @@ $(document).ready(function () {
     //CSS classes
     hour.addClass("hour col-sm-1");
     description.addClass("description col-sm-10");
-    saveBtn.addClass("saveBtn col-sm-1");
+    saveBtn.addClass("saveBtn col-sm-1 fas fa-save fa-lg");
     row.addClass("row time-block");
 
     /*hour.attr("timeAttribute", timeDisplay[i].value);
     description.attr("descriptionAttribute", timeDisplay[i].value);
     saveBtn.attr("buttonAttribute", timeDisplay[i].value);*/
 
-    //hour.text(timeDisplay[i].display);
+    hour.text(timeDisplay[i]);
     //description.text(timeDisplay[i].display);
     //saveBtn.text(timeDisplay[i].display);
 
@@ -34,17 +35,18 @@ $(document).ready(function () {
     $(".container").append(row);
 
     //Rows change color based on time
-    if (timeDisplay < currentDay) {
+    if (timeDisplay[i] < currentTime) {
       description.addClass("past");
-      // description.removeClass("future");
+      //description.removeClass("future");
       //description.removeClass("present");
-    } else if (timeDisplay === currentDay) {
+      console.log(currentTime);
+    } else if (timeDisplay[i] === currentTime) {
       //description.removeClass("past");
       //description.removeClass("future");
       description.addClass("present");
     } else {
       //description.removeClass("present");
-      // description.removeClass("past");
+      //description.removeClass("past");
       description.addClass("future");
     }
   }
